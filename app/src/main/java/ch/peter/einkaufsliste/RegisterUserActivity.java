5a90fee48176cbd42e1f71ded5669ba582376102
peter <peter.schweizer.info@gmail.com>
2021-11-18 11:04:55 +0100

    Register logic

--- app/src/main/java/ch/peter/einkaufsliste/RegisterUserActivity.java
+++ app/src/main/java/ch/peter/einkaufsliste/RegisterUserActivity.java
@@ -1,16 +1,23 @@
 package ch.peter.einkaufsliste;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUserActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -47,6 +54,83 @@ public class RegisterUserActivity extends AppCompatActivity implements View.OnCl
         {
             case R.id.banner:
                 startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+                break;
         }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+
+        if (fullName.isEmpty()) {
+            editTextFullName.setError("Voller Name wird benötigt!");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if (age.isEmpty()) {
+            editTextAge.setError("Alter wird benötigt!");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("E-Mail Addresse wird benötigt!");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("E-Mail Adresse ist nicht valide!");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Passwort wird benötigt!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Passwort benötigt mindestens 6 Zeichen!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            User user = new User(fullName, age, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(RegisterUserActivity.this, "Nutzer wurde erfolgreich registriert!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.VISIBLE);
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(RegisterUserActivity.this, "Nutzer registrieren fehlgeschlagen! Versuchen Sie es erneut!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.VISIBLE);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
     }
 }
