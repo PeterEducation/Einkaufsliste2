9277cf403d07c55ac4e2c448c1e992e0f36529d5
peter <peter.schweizer.info@gmail.com>
2021-11-18 16:41:13 +0100

    in progress

---
+++ app/src/main/java/ch/peter/einkaufsliste/PlannedShoppingsListActivity.java
@@ -0,0 +1,14 @@
+package ch.peter.einkaufsliste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlannedShoppingsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planned_shoppings_list);
+    }
+}
